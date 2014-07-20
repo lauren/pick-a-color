e9b7814cb18c4b7ecca8053bdfc323b9c6ad18ca
leider <derleider@web.de>
2014-07-21 00:19:45 +0200

    massive change to the build process

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,38 +1,64 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    copy: {
+      js: {
+        expand: true,
+        src: ['bower_components/jquery/dist/jquery.min.js', 'bower_components/tinycolor/tinycolor.js'],
+        dest: 'docs/js',
+        flatten: true
+      },
+      css: {
+        expand: true,
+        src: 'bower_components/bootstrap/dist/css/bootstrap.min.css',
+        dest: 'docs/css',
+        flatten: true
+      },
+      bootstrapLESS: {
+        cwd: 'bower_components/bootstrap/less/',
+        src: ['**'],
+        dest: 'build/less',
+        expand: true,
+        flatten: false
+      },
+      srcLESS: {
+        src: 'src/less/*less',
+        dest: 'build/less',
+        expand: true,
+        flatten: true
+      }
+    },
     uglify: {
       options: {
         banner: '/*! Pick-a-Color v<%= pkg.version %> | Copyright 2013 Lauren Sperber and Broadstreet Ads https://github.com/lauren/pick-a-color/blob/master/LICENSE | <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
-      my_target: {
-        files: {
-          'build/<%= pkg.version %>/js/<%= pkg.name %>-<%= pkg.version %>.min.js': ['src/js/<%= pkg.name %>.js'],
-          'docs/build/js/<%= pkg.name %>.min.js': ['src/js/<%= pkg.name %>.js']
-        }
-      }    },
-    less: {
       production: {
+        files: {
+          'build/js/<%= pkg.name %>.min.js': ['src/js/<%= pkg.name %>.js']
+        }
+      }
+    },
+    less: {
+      minify: {
         options: {
-          compress: true,
-          paths: ["src/less/css"]
+          cleancss: true,
+          report: 'min'
         },
         files: {
-          "build/<%= pkg.version %>/css/<%= pkg.name %>-<%= pkg.version %>.min.css": "src/less/<%= pkg.name %>.less",
-          "docs/build/css/<%= pkg.name %>.min.css": "src/less/<%= pkg.name %>.less"
+          "build/css/<%= pkg.name %>.min.css": "build/less/<%= pkg.name %>.less"
         }
       }
     },
     watch: {
       js: {
         files: ['src/js/*.js', 'Gruntfile.js'],
-        tasks: ['jshint', 'uglify'],
+        tasks: ['jshint', 'uglify']
       },
       less: {
         files: ['src/less/*.less', 'src/less/bootstrap-src/*.less'],
-        tasks: ['less'],
+        tasks: ['less']
       }
     },
     jshint: {
@@ -44,9 +70,13 @@ module.exports = function(grunt) {
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-bower-install-simple');
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['bower-install-simple', 'copy', 'jshint', 'less', 'uglify']);
 
 };
